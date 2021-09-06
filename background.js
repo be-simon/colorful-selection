7f9b4383f537e8e4c815264fb1d4f1936659bd24
정재용 <wodydthk7@hanyang.ac.kr>
2021-09-06 17:39:01 +0900

    수정: injected style이 DOM style에 덮어씌워지는 문제 해결

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-const colorSet = ["#000000", "#ffffff", "#ED8975", "#F2D096", "#8FB9AA", "#304D63"]
+const colorSet = ["#000000", "#ED8975", "#F2D096", "#8FB9AA", "#304D63", "#D2D8FB"]
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({"colorSet": colorSet})
-  chrome.storage.sync.set({"defaultColor": colorSet[3]})
+  chrome.storage.sync.set({"defaultColor": colorSet[1]})
 })
--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-const root = document.documentElement
-let currentColor = '#000000'
+let currentBackgroundColor = '#000000'
+let currentColor = '#ffffff'
+
+const styleSheet = document.styleSheets[document.styleSheets.length-1]
+
+function setSelectionBackground(backgroundColor, color) {
+  const selector = '::selection'
+  const cssText = `{background: ${backgroundColor}; color:${color}}`
+  styleSheet.insertRule(selector + cssText, styleSheet.cssRules.length)
+}
 
 chrome.storage.sync.get("defaultColor", ({defaultColor}) => {
-  currentColor = defaultColor
-  root.style.setProperty('--selected-color', currentColor)    
+  currentBackgroundColor = defaultColor
+  setSelectionBackground(currentBackgroundColor, currentColor)
 })
 
 chrome.runtime.onMessage.addListener(req => {
   if (req.message === 'change_color') {
-    currentColor = req.color
-    root.style.setProperty('--selected-color', currentColor)    
+    currentBackgroundColor = req.color
+    setSelectionBackground(currentBackgroundColor, currentColor)
   }
 })
